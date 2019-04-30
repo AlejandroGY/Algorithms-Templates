76c885bae32eb4706d94e2cda4ea3f0e9edfbe96
Alejandro González Yáñez <alejandro.gy96@gmail.com>
2019-04-29 23:56:38 -0500

    Create divisores_raiz3.cpp

---
+++ divisores_raiz3.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+int main( ) {
+   int n;
+   std::cin >> n;
+
+   std::vector<int> divisores;
+   auto raiz = std::sqrt(n);
+   for (int i = 1; i <= raiz; ++i) {
+      if (n % i == 0) {
+         if (n / i == i) {
+            divisores.push_back(i);
+         } else {
+            divisores.push_back(i);
+            divisores.push_back(n / i);
+         }
+      }
+   }
+   std::sort(divisores.begin( ), divisores.end( ));
+
+   std::cout << divisores.size( ) << "\n";
+   for (auto d : divisores) {
+      std::cout << d << " ";
+   }
+   std::cout << "\n";
+}
