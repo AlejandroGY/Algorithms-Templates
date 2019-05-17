3fd6bf040da04a0bb19518f73499b05423d32fa6
Alejandro González Yáñez <alejandro.gy96@gmail.com>
2019-05-17 16:36:16 -0500

    Update euclides.cpp

--- euclides.cpp
+++ euclides.cpp
@@ -5,18 +5,16 @@ long long gcd(long long a, long long b) {
    return (a == 0 ? b : gcd(b % a, a));
 }
 
-long long gcd_ext(long long a, long long b, long long* x, long long* y) {
+long long gcd_ext(long long a, long long b, long long& x, long long& y) {
    if (a == 0) {
-      *x = 0;
-      *y = 1;
+      x = 0, y = 1;
       return b;
    }
 
    long long x1, y1;
-   long long gcd = gcd_ext(b % a, a, &x1, &y1);
-
-   *x = y1 - (b / a) * x1;
-   *y = x1;
+   long long gcd = gcd_ext(b % a, a, x1, y1);
+   x = y1 - (b / a) * x1;
+   y = x1;
 
    return gcd;
 }
